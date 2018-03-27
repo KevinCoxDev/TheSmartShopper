dd55a51eab00cee83766097da3353a4c83343964
Kevin Cox <kevinpetercox@gmail.com>
2018-03-27 20:25:48 +0100

    Appearence Change Debugger Handshake issue

--- FragmentListAvailible.java
+++ FragmentListAvailible.java
@@ -1,15 +1,19 @@
 package kevin.cox.thesmartshopper;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -18,11 +22,13 @@ import java.util.Locale;
  * Created by kevin on 19/02/2018.
  */
 
-public class FragmentList extends Fragment {
+public class FragmentListAvailible extends Fragment implements RecyclerViewAdapterList.ItemClickListener {
 
 
-    EditText editsearch;
-    RecyclerViewAdapterList adapter;
+    private EditText editsearch;
+    private RecyclerViewAdapterList adapter;
+
+    private RecyclerViewAdapterList.ItemClickListener listener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,15 +37,14 @@ public class FragmentList extends Fragment {
         View view = inflater.inflate(R.layout.fragment_list, container, false);
 
         //Call populateList method. Creates thread to populate list from DB
-        ArrayList<ShopItem> jsonList = populateList();
-
+        ArrayList<ShopItem> itemList = populateList();
+        listener = this;
         RecyclerView recyclerView = view.findViewById(R.id.simpleListView);
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(),2));
-        adapter = new RecyclerViewAdapterList(getActivity(), jsonList);
+        adapter = new RecyclerViewAdapterList(getActivity(), itemList, listener);
         recyclerView.setAdapter(adapter);
 
         getActivity().findViewById(R.id.switcher_id).setVisibility(View.VISIBLE);
-
 
 
 
@@ -79,4 +84,36 @@ public class FragmentList extends Fragment {
         return db.getAllItems("items");
     }
 
+    @Override
+    public void onItemClick(int position) {
+
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(getActivity());
+            String message = String.valueOf(adapter.getItemId(position));
+            Log.d("ON CLICK", message);
+            final ShopItem item = adapter.getSelectedItem(position);
+            builder1.setMessage(item.getItemName().toUpperCase());
+            builder1.setCancelable(true);
+
+            builder1.setPositiveButton(
+                    "Add To My List",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            ItemDataBaseHandler db =  new ItemDataBaseHandler(getActivity());
+                            db.increaseQuantiy(item.getItemId(),"items");
+                            Toast.makeText(getActivity(), "Added to My List", Toast.LENGTH_SHORT).show();
+                            dialog.cancel();
+                        }
+                    });
+
+            builder1.setNegativeButton(
+                    "Cancel",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+    }
 }
